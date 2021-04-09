053bec16855d47b41519ff39d5a6183bfe36e094
fangnan <dongfangnan@gokuaidian.com>
2021-04-09 16:06:00 +0800

    first

---
+++ src/dian.lua
@@ -0,0 +1,21 @@
+tab1 = { key1 = "val1", key2 = "val2", "val3" }
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+
+tab1.key1 = nil
+for k, v in pairs(tab1) do
+    print(k .. " - " .. v)
+end
+
+
+a = {}
+a["key"] = "value"
+key = 10
+a[key] = 22
+a[key] = a[key] + 11
+for k, v in pairs(a) do
+    print(k .. " : " .. v)
+end
+
+
